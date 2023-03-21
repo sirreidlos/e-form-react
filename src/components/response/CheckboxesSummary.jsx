a9f8945d01a489ab28860e5517de2c5d3a6d2f95
Erik Sanjaya <erik.sanjaya25@gmail.com>
2023-03-21 15:05:32 +0700

    feat: chart for checkboxes

--- src/components/response/CheckboxesSummary.jsx
+++ src/components/response/CheckboxesSummary.jsx
@@ -1,67 +1,73 @@
-import React from 'react';
-import { Pie } from 'react-chartjs-2';
+import { useState } from "react";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
+import { useEffect } from "react";
+import { Pie } from "react-chartjs-2";
 
-export default function CheckboxesResponse({ number, data }) {
-    const [selectedOptions, setSelectedOptions] = useState([]);
+import { Doughnut } from "react-chartjs-2";
+export default function CheckboxesResponse({ number, text, data }) {
+  const response = {
+    "STEM-related": 2,
+    Music: 2,
+    Art: 10,
+    Gaming: 2,
+  };
 
-    useEffect(() => {
-    setSelectedOptions(answer.selected_options);
-    }, [answer]);
-    return (
-        <>
-        <div className="space-y-2">
-            <TextareaAutosize
-            type="text"
-            className="text-2xl w-full bg-transparent border-b border-gray-300 py-2 outline-none resize-none"
-            value={text}
-            placeholder="Question"
-            readOnly
-            />
-            {options.map((option, index) => {
-            return (
-                <div key={index}>
-                <input
-                    type="checkbox"
-                    value={option}
-                    checked={selectedOptions.indexOf(option) > -1}
-                    disabled
-                    readOnly
-                />
-                {option}
-                </div>
-            );
-            })}
-        </div>
-        </>
-    );
-    }
+  const chartData = {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
+  for (const [key, value] of Object.entries(data)) {
+    chartData.labels.push(key);
+    chartData.datasets[0].data[chartData.labels.indexOf(key)] = value;
+  }
+  return (
+    <>
+      <div className="space-y-2">
+        <TextareaAutosize
+          type="text"
+          className="text-2xl w-full bg-transparent border-b border-gray-300 py-2 outline-none resize-none"
+          value={text}
+          placeholder="Question"
+          readOnly
+        />
+        <Pie data={chartData} />
+      </div>
+    </>
+  );
+}
 
 const data = {
-    labels: ['Red', 'Green', 'Blue'],
-    datasets: [
-        {
-        data: [300, 50, 100],
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-        }
-    ]   
-    };
+  labels: ["Red", "Green", "Blue"],
+  datasets: [
+    {
+      data: [300, 50, 100],
+      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+    },
+  ],
+};
 
-    const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    legend: {
-        position: 'bottom',
-    }
-    };
-    const PieChart = () => {
-    return (
-        <div className="w-full max-w-lg mx-auto">
-        <Pie data={data} options={options} />
-        </div>
-    );
-    };
-
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  legend: {
+    position: "bottom",
+  },
+};
+const PieChart = () => {
+  return (
     <div className="w-full max-w-lg mx-auto">
-    <Pie data={data} options={options} className="rounded-lg shadow-md" />
+      <Pie data={data} options={options} />
     </div>
+  );
+};
+
+<div className="w-full max-w-lg mx-auto">
+  <Pie data={data} options={options} className="rounded-lg shadow-md" />
+</div>;
--- src/components/response/QuestionResponse.jsx
+++ src/components/response/QuestionResponse.jsx
@@ -1,18 +1,28 @@
-import CheckboxesQuestion from "./CheckboxesResponse";
-import TextAnswerQuestion from "./TextAnswerResult";
-import MultipleChoiceQuestion from "./MultipleChoiceResult";
-import DropdownQuestion from "./DropdownResult";
-import TimeQuestion from "./TimeResult";
-import DateQuestion from "./DateResult";
-import { useEffect } from "react";
+import CheckboxesResponse from "./CheckboxesResponse";
+import TextAnswerResponse from "./TextAnswerResult";
+import MultipleChoiceResponse from "./MultipleChoiceResult";
+import DropdownResponse from "./DropdownResult";
+import TimeResponse from "./TimeResult";
+import DateResponse from "./DateResult";
+import { Chart, ArcElement, Tooltip, Legend, Colors } from "chart.js";
+import CheckboxesSummary from "./CheckboxesSummary";
+// import DateSummary from "./DateSummary";
+// import DropdownSummary from "./DropdownSummary";
+// import MultipleChoiceSummary from "./MultipleChoiceSummary";
+// import TextAnswerSummary from "./TextAnswerSummary";
+// import TimeSummary from "./TimeSummary";
 
-export default function Question({
+Chart.register(ArcElement, Tooltip, Legend, Colors);
+
+export default function Response({
   number,
   text,
   kind,
   options,
   answer,
   responseId,
+  isSummaryMode = false,
+  summarizedData,
 }) {
   const questionType = {
     TextAnswer: "TextAnswer",
@@ -23,14 +33,20 @@ export default function Question({
     Time: "Time",
   };
 
+  console.log(summarizedData);
+
   return (
     <>
       <div className="bg-white rounded-lg p-8 space-y-8 border border-gray-300">
-        {kind === questionType.TextAnswer && (
-          <TextAnswerQuestion number={number} text={text} />
+        {kind === questionType.TextAnswer && !isSummaryMode && (
+          <TextAnswerResponse
+            number={number}
+            text={text}
+            answer={answer.input}
+          />
         )}
-        {kind === questionType.MultipleChoice && (
-          <MultipleChoiceQuestion
+        {kind === questionType.MultipleChoice && !isSummaryMode && (
+          <MultipleChoiceResponse
             number={number}
             text={text}
             options={options}
@@ -38,30 +54,63 @@ export default function Question({
             responseId={responseId}
           />
         )}
-        {kind === questionType.Checkboxes && (
-          <CheckboxesQuestion
+        {kind === questionType.Checkboxes && !isSummaryMode && (
+          <CheckboxesResponse
             number={number}
             text={text}
             options={options}
             answer={answer}
           />
         )}
-        {kind === questionType.Dropdown && (
-          <DropdownQuestion
+        {kind === questionType.Dropdown && !isSummaryMode && (
+          <DropdownResponse
             number={number}
             text={text}
             options={options}
             answer={answer}
           />
         )}
-        {kind === questionType.Date && (
-          <DateQuestion number={number} text={text} answer={answer} />
+        {kind === questionType.Date && !isSummaryMode && (
+          <DateResponse number={number} text={text} answer={answer} />
         )}
-        {kind === questionType.Time && (
-          <TimeQuestion number={number} text={text} answer={answer} />
+        {kind === questionType.Time && !isSummaryMode && (
+          <TimeResponse number={number} text={text} answer={answer} />
         )}
+
+        {/* {kind === questionType.TextAnswer && isSummaryMode && (
+          <TextAnswerSummary number={number} text={text} />
+        )}
+        {kind === questionType.MultipleChoice && isSummaryMode && (
+          <MultipleChoiceSummary
+            number={number}
+            text={text}
+            options={options}
+            answer={answer}
+            responseId={responseId}
+          />
+        )} */}
+        {kind === questionType.Checkboxes && isSummaryMode && (
+          <CheckboxesSummary
+            number={number}
+            text={text}
+            data={summarizedData}
+          />
+        )}
+        {/* {kind === questionType.Dropdown && isSummaryMode && (
+          <DropdownSummary
+            number={number}
+            text={text}
+            options={options}
+            answer={answer}
+          />
+        )}
+        {kind === questionType.Date && isSummaryMode && (
+          <DateSummary number={number} text={text} answer={answer} />
+        )}
+        {kind === questionType.Time && isSummaryMode && (
+          <TimeSummary number={number} text={text} answer={answer} />
+        )} */}
       </div>
     </>
-    
   );
 }
