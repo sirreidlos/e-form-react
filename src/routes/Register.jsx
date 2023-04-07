e92196f00b2beecf6bf5b1f7e86a33aa615de432
Erik-Sanjaya <erik.sanjaya25@gmail.com>
2023-04-08 05:01:26 +0700

    fix: login register

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -52,53 +52,52 @@ export default function Register() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-200">
-    <div className="container mx-auto">
-      <div className="flex w-8/12 bg-white absolute rounded-xl mx-auto shadow-lg overflow-hidden">
-    <div className="p-16 h-[36rem] flex-col flex gap-y-8 items-center bg-white rounded-2xl w-1/2">
+    <div className="h-screen w-screen flex justify-center items-center bg-gray-200">
+      <div class="p-16 h-[38rem] flex-col flex gap-y-8 items-center absolute bg-gradient-to-r w-[32rem]  from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-12 sm:rounded-3xl left-96"></div>
+      <div className="p-16 h-[36rem] flex-col flex gap-y-8 items-center justify-center bg-white rounded-2xl absolute left-96">
         <img src="/E-Form.png" alt="e form" />
         <h1 className="text-3xl font-bold">Register</h1>
         <form
           onSubmit={handleSubmit}
           className="flex flex-col gap-y-6 [&>input]:border-b-2"
-          >
-          <div className="flex flex-col gap-y-4 [&>input]:border-b-2 opacity-75">
+        >
+          <div className="flex flex-col gap-y-6 [&>input]:border-b-2">
             <input
               type="text"
               name="username"
-              className="w-96 rounded"
+              className="w-96"
               placeholder="Username"
               required
-              />
+            />
             <input
               type="email"
               name="email"
-              className="w-96 rounded"
+              className="w-96"
               placeholder="Email"
               required
-              />
+            />
             <input
               type="password"
               name="password"
-              className="w-96 rounded"
+              className="w-96"
               placeholder="Password"
               required
-              />
+            />
             <input
               type="password"
               name="confirm_password"
-              className="w-96 rounded"
+              className="w-96 "
               placeholder="Confirm Password"
               required
-              />
+            />
           </div>
           {error ? <p className="text-red-600">{error}</p> : ""}
-          <p className="text-base">
+          <p>
             If you already have an account.{" "}
             <Link
               to="/login"
-              className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600 font-bold"
-              >
+              className="underline text-blue-600 hover:text-blue-800 visited:text-sky-600 font-bold"
+            >
               Login Here
             </Link>
             .
@@ -106,22 +105,12 @@ export default function Register() {
           <button
             className="bg-sky-600 text-white p-3 font-bold rounded-lg"
             type="submit"
-            >
+          >
             Submit
           </button>
         </form>
       </div>
-      <div className=" bg-gradient-to-bl from-orange-400 to-yellow-300 p-16 h-[36rem] text-white text-2xl text-center flex-col flex gap-y-8 items-center justify-center w-1/2">
-        <h1>Welcome</h1>
-        <p>Making your surveying easier</p>
-      
-      </div>
-      </div>
-      </div>
-      <img
-      src="brown.jpg"
-      alt="bg.png.jpg"
-      ></img>
+      <img src="brown.jpg" className="w-screen h-screen" alt="bg.jpg" />
     </div>
   );
 }
